62831ad9cabe284f659aa7dd03b75361ceedacaf
Hood <omer@hoodapp.co.uk>
2023-08-20 07:58:16 +0200

    Initial Commit

---
+++ Hood Swift/Hood_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hood_SwiftApp.swift
+//  Hood Swift
+//
+//  Created by Hood on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hood_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
